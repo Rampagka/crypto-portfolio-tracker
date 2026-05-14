cbed189bf19d795fc314e3920fc329b6572925a6
Rampage <fedyaevandrey@yandex.ru>
2026-05-14 06:42:14 +0500

    fix(safari): remove stacking context from layout wrapper, disable body bg effects

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="relative z-1 mx-auto min-h-screen max-w-[768px] md:max-w-[1100px]">
+    <div class="mx-auto min-h-screen max-w-[768px] md:max-w-[1100px]">
         <app-header />
         <div class="layout-content min-h-[calc(100dvh-65px)] px-4 pb-[40px]">
             <slot />
